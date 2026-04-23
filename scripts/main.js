a8b73271fdbffa4128e832d8b7455c1004ca5ff3
nuyoah <hxr5736@gmail.com>
2026-04-23 16:28:31 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/cat.png") {
+    myImage.setAttribute("src", "images/cat2.png");
+  } else {
+    myImage.setAttribute("src", "images/cat.png");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+myButton.onclick = function () {
+  setUserName();
+};
